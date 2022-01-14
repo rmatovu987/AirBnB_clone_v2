5beb9964ed7c747b0aca6c1c1039e3e238695f8f
rmatovu987 <rmatovu987@gmail.com>
2022-01-14 21:47:07 +0300

    ...

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -36,7 +36,7 @@ exec {'create index.html':
 
 # change owner of folder /date recursively
 exec {'chown-data':
-  command  => 'sudo chown -hR ubuntu:ubuntu /data; sudo chown -hR ubuntu:ubuntu /data/web_static/',
+  command  => 'sudo chown -hR ubuntu:ubuntu /data; sudo chown -hR ubuntu:ubuntu /data/web_static',
   provider => shell,
 }
 
