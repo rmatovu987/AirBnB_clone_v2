2003c45c17587a113e3cabc89dc459f5fd6f4e0b
rmatovu987 <rmatovu987@gmail.com>
2022-02-02 01:38:26 +0300

    ...

--- models/city.py
+++ models/city.py
@@ -8,9 +8,12 @@ from models.base_model import Base, BaseModel
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
-    places = relationship("Place", backref="cities", cascade="all")
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ''
+        state_id = ''
--- models/state.py
+++ models/state.py
@@ -14,12 +14,15 @@ class State(BaseModel, Base):
     """ State class """
 
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="all")
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
 
     else:
+        name = ''
+        
+        @property
         def cities(self):
             """ Return the list of City instances with state_id """
             from models import storage
